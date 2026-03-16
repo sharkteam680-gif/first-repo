6396f9da4feaed2a64443b00c9f57d0a05b78e53
Mohamed <mahadaalin@gmail.com>
2026-03-16 13:42:51 +0300

    commit#2
    Add a JTextArea where users can type text (make it scrollable using
    JScrollPane), Add a JButton with the text &quot;Submit&quot;,    configuring the properties of a top-level window, typically using the JFrame class in the swing library.

--- src/Layout/BorderLayoutDemo.java
+++ src/Layout/BorderLayoutDemo.java
@@ -20,7 +20,40 @@ public class BorderLayoutDemo {
         Frame.setSize(600, 400);
         Frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         Frame.setLayout(new BorderLayout());
+
+        Frame.add(this.CreateTitleLabel(), BorderLayout.NORTH);
+        Frame.add(this.CreateTextArea(), BorderLayout.CENTER);
+        Frame.add(this.CreateScrollPane(), BorderLayout.WEST);
+        Frame.add(this.CreateSubmitButton(), BorderLayout.SOUTH);
+        Frame.add(this.CreatePanel(), BorderLayout.WEST);
         Frame.setVisible(true);
         return Frame;
+    }
+    public JLabel CreateTitleLabel() {
+        TitleLabel = new JLabel("Application Title", SwingConstants.CENTER);
+        return TitleLabel;
+    }
+
+    public JTextArea CreateTextArea() {
+        TextArea = new JTextArea();
+        return TextArea;
+    }
+
+    public JPanel CreatePanel() {
+        WestPanel = new JPanel(new GridLayout(3, 0));
+        WestPanel.add(new JButton("Option 1"));
+        WestPanel.add(new JButton("Option 2"));
+        WestPanel.add(new JButton("Option 3"));
+        return WestPanel;
+    }
+
+    public JScrollPane CreateScrollPane() {
+        ScrollPane = new JScrollPane(CreateTextArea());
+        return ScrollPane;
+    }
+
+    public JButton CreateSubmitButton() {
+        SubmitButton = new JButton("Submit");
+        return SubmitButton;
     }
 }
